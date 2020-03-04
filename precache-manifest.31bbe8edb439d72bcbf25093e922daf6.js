e8eb936476faffb44e16265a0a69335d7b9ce574
Vagan Mkrtchyan <perfoma777@mail.ru>
2020-03-04 15:05:35 +0400

    Updates

---
+++ precache-manifest.31bbe8edb439d72bcbf25093e922daf6.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb10018e28bf155b9eb8f6dc7f658e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25c1b93bfd1d09b2acec",
+    "url": "/static/css/2.8623a040.chunk.css"
+  },
+  {
+    "revision": "11b47c74840bb4681f71",
+    "url": "/static/css/3.f222e6ca.chunk.css"
+  },
+  {
+    "revision": "464c74b14c119b843dc8",
+    "url": "/static/css/4.e6610314.chunk.css"
+  },
+  {
+    "revision": "ba68b1f7a2847dca8766",
+    "url": "/static/css/main.ee5eee06.chunk.css"
+  },
+  {
+    "revision": "25c1b93bfd1d09b2acec",
+    "url": "/static/js/2.1b166ae0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1b166ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b47c74840bb4681f71",
+    "url": "/static/js/3.43dc8fe7.chunk.js"
+  },
+  {
+    "revision": "464c74b14c119b843dc8",
+    "url": "/static/js/4.01925739.chunk.js"
+  },
+  {
+    "revision": "ba68b1f7a2847dca8766",
+    "url": "/static/js/main.9555d976.chunk.js"
+  },
+  {
+    "revision": "78a656c9966b8daf56ce",
+    "url": "/static/js/runtime-main.ecb20936.js"
+  },
+  {
+    "revision": "c56fe719ddeae9799667600c8c4bbec1",
+    "url": "/static/media/bg.c56fe719.jpg"
+  },
+  {
+    "revision": "d2c893f55930c7cb5bfe41538be295d7",
+    "url": "/static/media/default-avatar.d2c893f5.png"
+  },
+  {
+    "revision": "d99368ca4e133a9917838e76243e8cfb",
+    "url": "/static/media/facebook.d99368ca.svg"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/static/media/github.192a6620.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/static/media/instagram.5b23a837.svg"
+  },
+  {
+    "revision": "e93d576182b97d2e874ee8137dbb4af7",
+    "url": "/static/media/mainlink.e93d5761.svg"
+  },
+  {
+    "revision": "28978814f9d6cfcf73a20696ab0163f7",
+    "url": "/static/media/news-blocker.28978814.jpg"
+  },
+  {
+    "revision": "5b11b33eb2ade7cdf61fbf4bf392a63d",
+    "url": "/static/media/spinner.5b11b33e.svg"
+  },
+  {
+    "revision": "1db9e9c352df34dd25e86db386f3d62b",
+    "url": "/static/media/twitter.1db9e9c3.svg"
+  },
+  {
+    "revision": "d4b26e2e5961b69171b7ef3a03b802ba",
+    "url": "/static/media/vk.d4b26e2e.svg"
+  },
+  {
+    "revision": "4e84a27e5632393900c77917ea199ae5",
+    "url": "/static/media/website.4e84a27e.svg"
+  },
+  {
+    "revision": "e571414bc6d2225623817f3ded84ca3e",
+    "url": "/static/media/youtube.e571414b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-network/precache-manifest.7f855aa34ad5c21844d7714244595ff7.js"
+  "/precache-manifest.31bbe8edb439d72bcbf25093e922daf6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-network/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
