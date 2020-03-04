9e0377f63780e47ec64a0513728b58ea08749279
Vagan Mkrtchyan <perfoma777@mail.ru>
2020-03-04 16:11:15 +0400

    secondary commits

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.scss';
+import './index.css';
 import { HashRouter } from 'react-router-dom';
 import FunctionRender from './App';
 
