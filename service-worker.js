d3d5d8e7386ff8476b440b61c1da3b522275079d
EsaxCO <59527678+EsaxCO@users.noreply.github.com>
2020-03-05 14:47:25 +0400

    edit modes for profile and avatar

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-network/precache-manifest.57c897fd3e8f633288db02df984d1210.js"
+  "/social-network/precache-manifest.904e55bcd348746b33e1e30500abd243.js"
 );
 
 self.addEventListener('message', (event) => {
